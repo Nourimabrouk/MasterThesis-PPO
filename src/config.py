03bea22404cc229a2c0fd7eb0db3d462ede5e080
Nouri <46678756+NM@users.noreply.github.com>
2023-12-21 23:04:15 +0100

    .

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ NUM_RUNS = 1 # 100 used for the paper
 EVALUATION_EPISODES = 10
 TOTAL_TIMESTEPS = 1000000 # 1000000 used for the paper
 
-LOG_PATH = './output/tensorboard_logs/'
-SAVE_PATH = './output/models/'	
+LOG_PATH = 'output/tensorboard_logs/'
+SAVE_PATH = 'output/models/'	
 ENVIRONMENTS = ["CartPole-v1", "FrozenLake-v1", "MountainCar-v0"]
 AGENTS = ["PPO", "A2C", "DQN"] 
 
